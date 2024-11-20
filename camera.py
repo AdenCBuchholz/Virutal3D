27f54136ddd0480838b6ed0cb3fda1cd797fd121
Aden Buchholz <98045023+AdenCBuchholz@users.noreply.github.com>
2024-11-20 13:10:46 -0500

    Mininmal webcam use example

---
+++ camera.py
@@ -0,0 +1,29 @@
+#camera.py
+# Learn to read video, display video, and save video.
+# Learn to capture video from a camera and display it.
+# You will learn these functions : cv.VideoCapture(), cv.VideoWriter()
+import numpy as np
+import cv2 as cv
+ 
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+ 
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()

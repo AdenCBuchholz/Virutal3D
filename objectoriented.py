b66a09ef3b9bf6c6f3f4fcaa4e0ceace66176210
Aden Buchholz <98045023+AdenCBuchholz@users.noreply.github.com>
2024-11-25 12:39:38 -0500

    Added functionality to OOP Game

--- objectoriented.py
+++ objectoriented.py
@@ -1,3 +1,12 @@
-#This will be an object oriented version
-#of the virtual3d game
-print('Starting 00 Virtual3d')
+# This will be an object oriented version
+# of the virtual3d game
+
+class Facefinder:
+#Use haarcascade filter to detect largest face from a frame.
+    def __init__ (self):
+        print ('Face Finder Initalize')
+
+#---------------------------------------------------------------
+# Main
+ff = Facefinder()
+print("Virtual3d Complete")
